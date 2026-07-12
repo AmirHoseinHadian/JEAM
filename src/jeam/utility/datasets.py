da1473c558f7efd431e9edd00d5f05274d7dbca5
AmirHoseinHadian <amir.h.hadian@gmail.com>
2026-07-12 21:39:33 +0200

    refactor dataloading functions

--- src/jeam/utility/datasets.py
+++ src/jeam/utility/datasets.py
@@ -1,8 +1,14 @@
 import os
 import numpy as np
 import pandas as pd
+from importlib import resources
 
 __dir__ = os.path.abspath(os.path.dirname(__file__))
+
+def _read_csv(filename, **kwargs):
+    resource = resources.files("jeam").joinpath("data", filename)
+    with resources.as_file(resource) as data_path:
+        return pd.read_csv(data_path, **kwargs)
 
 def load_fennell2023():
     '''
@@ -13,11 +19,7 @@ def load_fennell2023():
     pd.DataFrame
         A dataframe containing the data with columns: 'subjectNumber', 'blockNumber', 'trialNumber', 'rt', 'numberOfStimulus', 'responseError'
     '''
-
-    data_path = os.path.join(os.path.dirname(os.path.dirname(__dir__)), "data", "Fennell2023_exp4.csv")
-
-    data = pd.read_csv(data_path, index_col=0)
-    return data
+    return _read_csv("Fennell2023_exp4.csv", index_col=0)
 
 
 def load_kvam2019():
@@ -29,15 +31,18 @@ def load_kvam2019():
     pd.DataFrame
         A dataframe containing the data with columns: 
     '''
-
-    data_path = os.path.join(os.path.dirname(os.path.dirname(__dir__)), "data", "Kvam2019.csv")
-
-    data = pd.read_csv(data_path)
-
-    data = data.drop(columns=['isCued', 'cueOrientation', 
-                                'cueDeflections', 'points', 
-                                'jitterLevel', 'targetOrientation'])
-    data = data.rename(columns={'RT':'rt'})
-    data.deviation *= 2
-    data.absoluteDeviation = np.abs(data.deviation)
+    data = _read_csv("Kvam2019.csv")
+    data = data.drop(
+        columns=[
+            "isCued",
+            "cueOrientation",
+            "cueDeflections",
+            "points",
+            "jitterLevel",
+            "targetOrientation",
+        ]
+    )
+    data = data.rename(columns={"RT": "rt"})
+    data["deviation"] *= 2
+    data["absoluteDeviation"] = np.abs(data["deviation"])
     return data
